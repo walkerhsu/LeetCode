75e29018ec2afaf50e222e8ecf067b41b1c694f1
walkerhsu <walkerhsu0808@gmail.com>
2024-04-14 01:40:39 +0800

    problem 283

---
+++ Two_pointer/283.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int first_zero = -1;
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i] != 0 && first_zero != -1) {
+                int tmp = nums[i];
+                nums[i] = nums[first_zero];
+                nums[first_zero] = tmp;
+                first_zero ++;
+            } else if (nums[i] == 0 && first_zero == -1) {
+                first_zero = i;
+            }
+        }
+    }
+};
